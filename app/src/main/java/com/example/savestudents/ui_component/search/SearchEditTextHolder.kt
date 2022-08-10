312c4d4f5f8e2a775f665e3bd21fe7b96d5927fb
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-08-10 02:49:25 -0300

    feat: set Search EditText Holder to default layout

--- app/src/main/java/com/example/savestudents/ui_component/search/SearchEditTextHolder.kt
+++ app/src/main/java/com/example/savestudents/ui_component/search/SearchEditTextHolder.kt
@@ -1,23 +1,96 @@
 package com.example.savestudents.ui_component.search
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.example.savestudents.R
 
 @EpoxyModelClass
-abstract class SearchEditTextHolder: EpoxyModelWithHolder<SearchEditTextHolder.SectionHolder>() {
+abstract class SearchEditTextHolder : EpoxyModelWithHolder<SearchEditTextHolder.SectionHolder>() {
+
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    lateinit var clickFilterButton: () -> Unit
 
     override fun bind(holder: SectionHolder) {
         super.bind(holder)
+        holder.apply {
+            handleClickFilterButton()
+            clickButtonCancel()
+            handleOnFocusEditText()
+        }
     }
 
-    inner class SectionHolder: EpoxyHolder() {
-        override fun bindView(itemView: View) {
-
+    private fun SectionHolder.handleClickFilterButton() {
+        mButtonFilterContainer.setOnClickListener {
+            clickFilterButton()
         }
+    }
 
+    private fun SectionHolder.clickButtonCancel() {
+        mButtonCancel.setOnClickListener { view ->
+            mEditText.apply {
+                clearFocus()
+                text.clear()
+            }
+            setDefaultBackgroundEditText(view.context)
+        }
+    }
+
+    private fun SectionHolder.handleOnFocusEditText() {
+        mEditText.setOnFocusChangeListener { view, _ ->
+            setBackgroundClickEditText(view.context)
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun SectionHolder.setDefaultBackgroundEditText(context: Context) {
+        mSearchBarContainer.setBackgroundDrawable(context.getDrawable(R.drawable.bg_rounded_blue_light))
+        visibleButtonFilter()
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun SectionHolder.setBackgroundClickEditText(context: Context) {
+        visibleClearEditText()
+        mSearchBarContainer.setBackgroundDrawable(context.getDrawable(R.drawable.bg_rounded_blue_light_with_border_primary))
+    }
+
+    private fun SectionHolder.visibleButtonFilter() {
+        mButtonCancel.visibility = View.GONE
+        mButtonFilterContainer.visibility = View.VISIBLE
+        mButtonFilterIcon.visibility = View.VISIBLE
+        mButtonFilterText.visibility = View.VISIBLE
+    }
+
+    private fun SectionHolder.visibleClearEditText() {
+        mButtonCancel.visibility = View.VISIBLE
+        mButtonFilterContainer.visibility = View.GONE
+        mButtonFilterIcon.visibility = View.GONE
+        mButtonFilterText.visibility = View.GONE
+    }
+
+    inner class SectionHolder : EpoxyHolder() {
+        lateinit var mSearchBarContainer: View
+        lateinit var mEditText: EditText
+        lateinit var mButtonFilterContainer: View
+        lateinit var mButtonFilterIcon: ImageView
+        lateinit var mButtonFilterText: TextView
+        lateinit var mButtonCancel: TextView
+
+        override fun bindView(itemView: View) {
+            mSearchBarContainer = itemView.findViewById(R.id.search_bar_container)
+            mEditText = itemView.findViewById(R.id.search_bar)
+            mButtonFilterContainer = itemView.findViewById(R.id.filter_button_container)
+            mButtonFilterIcon = itemView.findViewById(R.id.filter_button_icon)
+            mButtonFilterText = itemView.findViewById(R.id.filter_button_title)
+            mButtonCancel = itemView.findViewById(R.id.button_cancel)
+        }
     }
 
     override fun getDefaultLayout(): Int {
