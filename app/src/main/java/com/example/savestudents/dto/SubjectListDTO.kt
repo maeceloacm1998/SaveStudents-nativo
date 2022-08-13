410aacc351f787f36254dbdfb048198041ea38ca
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-08-13 03:30:17 -0300

    feat: add shift props in SubjectListDTO

--- app/src/main/java/com/example/savestudents/dto/SubjectListDTO.kt
+++ app/src/main/java/com/example/savestudents/dto/SubjectListDTO.kt
@@ -5,7 +5,8 @@ import com.example.savestudents.model.SubjectList
 data class SubjectListDto(
     var id: Long? = 0,
     var title: String? = "",
-    var period: String? = ""
+    var period: String? = "",
+    var shift: String? = ""
 )
 
 fun List<SubjectListDto>.asDomainModel(): List<SubjectList> {
--- app/src/main/java/com/example/savestudents/model/SubjectList.kt
+++ app/src/main/java/com/example/savestudents/model/SubjectList.kt
@@ -3,5 +3,6 @@ package com.example.savestudents.model
 data class SubjectList(
     var id: Long? = 0,
     var title: String? = "",
-    var period: String? = ""
+    var period: String? = "",
+    var shift: String? = "",
 )
