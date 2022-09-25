b9a6c76f89654d1c83b6d757e2e1eb058d06c0ef
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-09-25 03:51:17 -0300

    feat: create activity_login screen

---
+++ app/src/main/java/br/com/savestudents/debug_mode/view/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.savestudents.debug_mode.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.savestudents.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
