e2e1993076b6f450f1d3848e17b4acffd763bcb9
marcelo.mendes <marcelo.cmendes@b2wdigital.com>
2023-01-25 00:16:44 -0300

    feat: create function to open app notification settings

--- app/core/src/main/java/com/br/core/notifications/NotificationsManager.kt
+++ app/core/src/main/java/com/br/core/notifications/NotificationsManager.kt
@@ -3,7 +3,10 @@ package com.br.core.notifications
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -36,5 +39,28 @@ class NotificationsManager(private var context: Context) : NotificationManagerCo
         with(NotificationManagerCompat.from(context)) {
             notify(idNotification, builder.build())
         }
+    }
+
+    override fun openAppNotificationSettings() {
+        val intent = Intent().apply {
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                    action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                    putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+                }
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> {
+                    action = "android.settings.APP_NOTIFICATION_SETTINGS"
+                    putExtra("app_package", context.packageName)
+                    putExtra("app_uid", context.applicationInfo.uid)
+                }
+                else -> {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    addCategory(Intent.CATEGORY_DEFAULT)
+                    data = Uri.parse("package:" + context.packageName)
+                }
+            }
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(intent)
     }
 }
